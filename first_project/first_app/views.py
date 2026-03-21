87ad599459d3171c74026583ca11276ba20297fe
Nico <nico99rueda@gmail.com>
2026-03-21 17:27:12 +0100

    First Commit

---
+++ first_project/first_app/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello World!")
+
+def home(request):
+    return HttpResponse("Welcome to home page!")
+
+def educative(request):
+    return HttpResponse("Welcome to Educative page!")
+
+def show_age(request, age):
+    return HttpResponse("I am %s years old." % age)
+
+def even_or_odd(request, num):
+    if(num%2==0):
+        output="%s is an even number." % num
+    else:
+        output="%s is an odd number." % num
+    return HttpResponse(output)
